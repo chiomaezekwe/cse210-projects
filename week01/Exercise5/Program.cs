334132c897dc476a28ed3b91936fe9a3323a0c2f
chiomaezekwe <chummyankie@gmail.com>
2025-01-18 23:42:54 +0100

    Finished week01 Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,52 @@ using System;
 
 class Program
 {
+    // The Function to display a welcome message.
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    // Function to ask users for their name and the output returned as a string.
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    // Function to ask users for their favorite number and the output is returned as an integer.
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    // Function to square the number provided by the users and the output returned as an integer.
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    // Function to display the result; ie, Output: name (a string) and the squared number (an integer).
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // Call DisplayWelcome Function to show the initial message.
+        DisplayWelcome();
+
+        // Call PromptUserName Function to get the user's name.
+        string userName = PromptUserName();
+
+        // Call PromptUserNumber Function to get the user's favorite number.
+        int favoriteNumber = PromptUserNumber();
+
+        // Call SquareNumber Function to calculate the square of the favorite number.
+        int squaredNumber = SquareNumber(favoriteNumber);
+
+        // Call DisplayResult Function to display the result to the user.
+        DisplayResult(userName, squaredNumber);
     }
 }
